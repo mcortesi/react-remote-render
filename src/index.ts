a7ae001debc0400e9037b4c0e2ca9d0f6c52b9c2
Mariano A. Cortesi <mariano@flowics.com>
2017-08-25 14:00:19 -0300

    Improve setup for package deployment

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export { default as RemoteRenderProvider } from './components/RemoteRenderProvider';
+export { default as Renderer } from './components/Renderer';
+export { default as withRemoteRender, RemoteRenderComponent } from './components/withRemoteRender';
+export * from './model/default-service';
+export * from './types/messages';
+export * from './types/service';
+export * from './types/transport';
+export * from './types/base';
