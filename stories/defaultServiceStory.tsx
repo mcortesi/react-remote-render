2a74d39b9a2e75a762970e17b628af786b5c0938
Mariano A. Cortesi <mariano@flowics.com>
2017-11-13 10:47:05 -0300

    Fix stories

--- stories/defaultServiceStory.tsx
+++ stories/defaultServiceStory.tsx
@@ -1,34 +1,37 @@
 import { storiesOf } from '@storybook/react';
 import * as React from 'react';
-import withRemoteRender, { RemoteRenderComponent } from '../src/components/withRemoteRender';
+import withRemoteRender, {
+  RemoteRenderComponent
+} from '../src/components/withRemoteRender';
 import { TestScenario } from './utils';
 
 const stories = storiesOf('With DefaultService', module);
 
 type ParagraphProps = { text: string };
-const Paragraph: React.SFC<ParagraphProps> = ({ text }) => (<p>{text}</p>);
+const Paragraph: React.SFC<ParagraphProps> = ({ text }) => <p>{text}</p>;
 
-
-type ButtonProps = { onClick: Function, n: number };
+type ButtonProps = { onClick: Function; n: number };
 const Button: React.SFC<ButtonProps> = ({ onClick, n }) => (
   <button onClick={e => onClick(n)}>Increment by {n}</button>
 );
 
-const RRParagraph = withRemoteRender<ParagraphProps>({ name: 'Paragraph'})(Paragraph);
-const RRButton = withRemoteRender<ButtonProps>({ name: 'Button'})(Button);
+const RRParagraph = withRemoteRender<ParagraphProps>({ name: 'Paragraph' })(
+  Paragraph
+);
+const RRButton = withRemoteRender<ButtonProps>({ name: 'Button' })(Button);
 
 class ButtonText extends React.Component {
-  state: { clicks: number } = { clicks: 0 }
+  state: { clicks: number } = { clicks: 0 };
 
-  incrementClicks = (n) => {
+  incrementClicks = n => {
     this.setState({ clicks: this.state.clicks + n });
-  }
+  };
 
   render() {
     return (
       <div>
-        <RRButton onClick={this.incrementClicks} n={-1}/>
-        <RRButton onClick={this.incrementClicks} n={2}/>
+        <RRButton onClick={this.incrementClicks} n={-1} />
+        <RRButton onClick={this.incrementClicks} n={2} />
         <RRParagraph text={`${this.state.clicks} clicks`} />
       </div>
     );
@@ -36,14 +39,16 @@ class ButtonText extends React.Component {
 }
 
 stories.add('Simple Example', () => (
-  <TestScenario supportedComponents={[RRParagraph]} >
-    <RRParagraph text="Hola bebe!" />
-    <RRParagraph text="adios mundo cruel" />
+  <TestScenario supportedComponents={[RRParagraph]}>
+    <div>
+      <RRParagraph text="Hola bebe!" />
+      <RRParagraph text="adios mundo cruel" />
+    </div>
   </TestScenario>
 ));
 
 stories.add('Calling Action in externalized components', () => (
-  <TestScenario supportedComponents={[RRParagraph, RRButton]} >
+  <TestScenario supportedComponents={[RRParagraph, RRButton]}>
     <ButtonText />
   </TestScenario>
 ));
